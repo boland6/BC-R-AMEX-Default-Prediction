fc86df51dc4cef83de7ef29066b027d684c3ea62
Patrick Boland <patrickboland@Patricks-MacBook-Pro.local>
2023-11-20 16:34:06 -0500

    Sampling function for large set of data without reading into memory

--- Data Sampling.R
+++ Data Sampling.R
@@ -8,14 +8,36 @@ library(ggformula)
 library(fastDummies)
 library(caret)
 
-
 #Ingesting data from AMEX
-df < -read.csv("train_data.csv")
+  #This contains the labels for the training data for the entire dataset
 df_labels <- read.csv("train_labels.csv")
 
-# Estimate the size of the full dataset in memory
-full_size_df <- object.size(df)
-full_size_df_labels <- object.size(df_labels)
+
+# Function to sample a large file without reading into memory
+
+sample_training_data <- function(file_path, sample_size_mb, chunk_size = 1000, seed = 23) {
+  
+  # Set the seed for reproducibility
+  set.seed(seed)
+  
+  # Estimate the number of lines in the file
+  initial_read <- fread(file_path, nrows = chunk_size)
+  file_size <- file.size(file_path)
+  lines_estimate <- nrow(initial_read) / (file_size / 15e+9) * file_size
+  
+  # Determine the number of lines to sample based on the desired sample size
+  avg_line_size <- file_size / lines_estimate
+  num_lines_to_sample <- (sample_size_mb * 1e+6) / avg_line_size
+  
+  # Create a vector of line numbers to sample
+  line_numbers_to_sample <- sort(sample(lines_estimate, num_lines_to_sample))
+  
+  # Read the sampled lines
+  sampled_data <- rbindlist(lapply(line_numbers_to_sample, function(ln) fread(file_path, skip = ln - 1, nrows = 1)))
+  
+  # Return the sampled data
+  return(sampled_data)
+}
 
 
 
